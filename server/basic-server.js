94419c3ccb52bb6e7f675a4b52ea762c0463e62f
kidmillions <claymanchris@gmail.com>
2015-06-02 17:05:31 -0700

    Refactored server code

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,20 +1,20 @@
 var handler = require('./request-handler.js');
-var static = require('node-static');
-var requests = require('request');
 var fs = require('fs');
 console.log(handler.name);
-
+var url = require('url');
 /* Import node's http module: */
 var http = require("http");
 var port = 3000;
 var ip = "127.0.0.1";
-var fileServer = new static.Server('../client');
 
-
-var server = http.createServer(handler.requestHandler);
+var server = http.createServer(function(request, response){
+  var routes = '/classes/messages';
+  var uri = url.parse(request.url).pathname;
+  if (uri === routes) {
+    handler.requestHandler(request, response);
+  } else {
+    handler.sendResponse(response, '404 not found', 404);
+  }
+});
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
-
-server.on('error', function(err) {
-  console.log('Server error', err.message);
-})
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,23 +1,17 @@
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  "content-type": "application/json"
+};
 
 var exports = module.exports = {};
 var url = require('url');
 var fs = require('fs');
 var path = require('path');
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
 var dataStorage = {results:[]};
-
+var objectId = 1;
 
     // var stubMsg = {
     //   username: 'Jono',
@@ -25,77 +19,49 @@ var dataStorage = {results:[]};
     // };
     // var req = new stubs.request('/classes/room1', 'POST', stubMsg);
 
-
-var requestHandler = function(request, response) {
-  var uri = url.parse(request.url).pathname;
-  // var filename = path.normalize('.' + request.url);
-  var headers = defaultCorsHeaders;
-  var statusCode = 200;
-  var res = '';
-
-
-
-  request.setEncoding('utf8');
-  headers['Content-Type'] = "application/JSON";
-
-  if (!(request.url === '/classes/messages' || request.url === '/')) {
-    response.writeHead(404, headers);
-    response.write('404 error');
-    console.log('should be 404')
-    response.end();
-    return;
-  }
-
-  if (request.method === "GET") {
-
-    var path = './data.json'
-    fs.open(path, 'r', function(err, fd) {
-      var readStream = fs.createReadStream(null, {fd: fd, encoding: 'utf8'});
-      var string = ''
-      readStream.on('data', function(chunk) {
-
-          string += chunk;
-          // response.write(JSON.stringify(chunk));
-          // response.write(JSON.stringify(chunk), 'utf8');
-          // console.log(response);
-          // response.write(JSON.stringify(chunk));
-
-        });
-      readStream.on('end', function() {
-        response.writeHead(statusCode, headers);
-        console.log(string);
-        response.end(string);
-        return;
-      })
-
-      });
-
-    // res = JSON.stringify(dataStorage);
-  }
-
-  if (request.method === "POST") {
-    request.on('data', function(chunk) {
-      // boot up the data.js file
-      dataStorage.results.push(JSON.parse(chunk));
+var actions = {
+  'GET': function (request, response) {
+    sendResponse(response, dataStorage, 200);
+  },
+  'POST': function (request, response) {
+    collectData(request, function(message) {
+      dataStorage['results'].push(message);
+      sendResponse(response, {objectId: objectId}, 201);
+      objectId++;
     });
-    statusCode = 201;
-  }
-
-  console.log("Serving request type " + request.method + " for url " + request.url);
-  console.log(res);
-  response.writeHead(statusCode, headers);
-  response.write(res);
-  response.end();
+  },
+ 'OPTIONS': function(request, response){
+     sendResponse(response);
+   }
 };
 
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
+var sendResponse = function(response, data, statusCode) {
+  var statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers);
+  response.end(JSON.stringify(data));
+};
+
+
+var collectData = function(request, callback) {
+  var data = '';
+  request.on('data', function(chunk) {
+    data += chunk;
+  });
+  request.on('end', function() {
+    callback(JSON.parse(data));
+  });
+};
+
+var requestHandler = function(request, response) {
+  var action = actions[request.method];
+  if (action) {
+    action(request, response);
+  } else {
+    throw new Error('action now found');
+  }
 };
 
 var name = 'nodeJS Server Written by Austin and Chris';
 exports.name = name;
 exports.requestHandler = requestHandler;
-exports.defaultCorsHeaders = defaultCorsHeaders;
+exports.sendResponse = sendResponse;
